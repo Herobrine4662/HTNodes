a79935a4b798b48afd24d42a150bf6a90506af9f
HTNodes <108970506+Herobrine4662@users.noreply.github.com>
2025-09-13 18:10:48 +0530

    Update and rename Panel-Install to panel.sh

--- panel.sh
+++ panel.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
+
 set -e
-
-# ---------------- Helper functions ---------------- #
-
-# Simple logging functions
-output() {
-    echo -e "[*] $1"
-}
-
-success() {
-    echo -e "[✔] $1"
-}
-
-warning() {
-    echo -e "[!] $1"
-}
 
 ######################################################################################
 #                                                                                    #
@@ -41,6 +27,14 @@ warning() {
 # https://github.com/pterodactyl-installer/pterodactyl-installer                     #
 #                                                                                    #
 ######################################################################################
+
+# Check if script is loaded, load if not or fail otherwise.
+fn_exists() { declare -F "$1" >/dev/null; }
+if ! fn_exists lib_loaded; then
+  # shellcheck source=lib/lib.sh
+  source /tmp/lib.sh || source <(curl -sSL "$GITHUB_BASE_URL/$GITHUB_SOURCE"/lib.sh)
+  ! fn_exists lib_loaded && echo "* ERROR: Could not load lib script" && exit 1
+fi
 
 # ------------------ Variables ----------------- #
 
