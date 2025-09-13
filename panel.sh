aa0a589d17f091de64902a6c62a6bbddb48666b2
HTNodes <108970506+Herobrine4662@users.noreply.github.com>
2025-09-13 18:16:12 +0530

    Update panel.sh

--- panel.sh
+++ panel.sh
@@ -29,16 +29,12 @@ set -e
 ######################################################################################
 
 # Download and source lib.sh dynamically
-curl -s -o /tmp/lib.sh https://raw.githubusercontent.com/Herobrine4662/HTNodes/main/lib.sh
-source /tmp/lib.sh
-
-# Check if script is loaded, load if not or fail otherwise.
-fn_exists() { declare -F "$1" >/dev/null; }
-if ! fn_exists lib_loaded; then
-  # shellcheck source=lib/lib.sh
-  source /tmp/lib.sh || source <(curl -sSL "$GITHUB_BASE_URL/$GITHUB_SOURCE"/lib.sh)
-  ! fn_exists lib_loaded && echo "* ERROR: Could not load lib script" && exit 1
+curl -fsSL https://raw.githubusercontent.com/Herobrine4662/HTNodes/main/lib.sh -o /tmp/lib.sh
+if [ ! -f /tmp/lib.sh ]; then
+    echo "* ERROR: Could not download lib.sh"
+    exit 1
 fi
+source /tmp/lib.sh
 
 # ------------------ Variables ----------------- #
 
